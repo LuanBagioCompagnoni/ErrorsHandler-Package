da3964cd7196d636f69753b7eec41da1747694b6
Luan Bagio Compagnoni <105982483+LuanBagioCompagnoni@users.noreply.github.com>
2024-09-03 17:38:30 -0300

    Adjust name of errors

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-export { default as DuplicateError } from './errors/DuplicityError.js';
-export { default as NotFoundError } from './errors/InternalNotFoundError.js';
+export { default as DuplicityError } from './errors/DuplicityError.js';
+export { default as InternalNotFoundError } from './errors/InternalNotFoundError.js';
 export { default as InvalidRequestError } from './errors/InvalidRequestError.js';
-export { default as NoChangeError } from './errors/InvalidRequestError';
+export { default as InvalidRequestError } from './errors/InvalidRequestError';
 export { default as TokenError } from './errors/TokenError.js';
 export { default as ValidationError } from './errors/ValidationError.js';
 
