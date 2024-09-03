71e7f7057051c13e272cf8bfde5536d6f7da56d0
Luan Bagio Compagnoni <105982483+LuanBagioCompagnoni@users.noreply.github.com>
2024-09-03 17:45:43 -0300

    Remove duplcated export

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 export { default as DuplicityError } from './errors/DuplicityError.js';
 export { default as InternalNotFoundError } from './errors/InternalNotFoundError.js';
 export { default as InvalidRequestError } from './errors/InvalidRequestError.js';
-export { default as InvalidRequestError } from './errors/InvalidRequestError';
 export { default as TokenError } from './errors/TokenError.js';
 export { default as ValidationError } from './errors/ValidationError.js';
 export { default as NoChangeError } from './errors/NoChangeError.js';
